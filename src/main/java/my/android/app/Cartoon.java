0263cc3874735a33a5a83f187cde2ac5bb5d92af
cmaldivi <cmaldivi@users.noreply.github.com>
2016-05-02 12:33:23 +0200

    Update Cartoon.java

--- src/main/java/my/android/app/Cartoon.java
+++ src/main/java/my/android/app/Cartoon.java
@@ -1,13 +1,4 @@
 package my.android.app;
-/*
- * Copyright (C) 2016 Orange
- * Authors: Christophe Maldivi
- *
- * This software is the confidential and proprietary information of Orange.
- * You shall not disclose such confidential information and shall use it only
- * in accordance with the terms of the license agreement you entered into
- * with Orange.
- */
 
 public class Cartoon {
 
